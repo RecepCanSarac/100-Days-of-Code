99a2d16cd25a011fc37a8665ba41f71809773460
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-04-17 23:50:34 +0300

    Band Name Generator Day1

---
+++ Day1/Band-Name-Genarator.py
@@ -0,0 +1,11 @@
+print("Welcome to the band name generator")
+
+city = input("Which city did you grow up in?\n")
+print(city)
+
+pet = input("What is the name of a pet?\n")
+
+print("Your band name could be " + city + " " + pet)
+
+
+
